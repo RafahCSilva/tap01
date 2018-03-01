502abd40a1fc353a6283b8e07a19377ecf77a552
RafahCSilva <rafahcsilva@gmail.com>
2018-03-01 00:51:35 -0300

    Possibilidade de uma coluna

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "Base.h"
+#include "Config.h"
 
 int main() {
     std::cout << "Eae Rafao" << std::endl;
 
+    // MUDANCA DE BASE
     std::cout << "Testando ID_b20 -> ID_t" << std::endl;
     ID_t a = Base::int2ID( 512 );
     std::cout << "  ( " << 512 << " ) -> " << a.id0 << " " << a.id1 << " " << a.id2 << std::endl;
@@ -12,6 +14,19 @@ int main() {
     ID_t b( 3, 2, 1 );
     int c = Base::ID2int( &b );
     std::cout << "  ( " << b.id0 << " " << b.id1 << " " << b.id2 << " ) -> " << c << std::endl;
+
+    // POSSIBILIDADE DA COLUNA
+    std::cout << "Possibilidades de Config da Coluna:" << std::endl;
+    int id = 0;
+    auto *configs = new config_t[tab_possib];
+    for ( int i = 0; i < tab_ALTURA; ++i )
+        for ( int j = tab_ALTURA - 1; j >= 0; --j )
+            if ( i != j ) {
+                configs[ id ] = config( id, i, j );
+                id++;
+            }
+    for ( int i = 0; i < tab_possib; ++i )
+        printConfig( &configs[ i ] );
 
     return 0;
 }
