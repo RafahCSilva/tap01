8e0713c914c3264e8ba18be754b2b3419648bc75
RafahCSilva <rafahcsilva@gmail.com>
2018-03-01 00:15:37 -0300

    add ID de base 20

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "Base.h"
 
 int main() {
     std::cout << "Eae Rafao" << std::endl;
+
+    std::cout << "Testando ID_b20 -> ID_t" << std::endl;
+    ID_t a = Base::int2ID( 512 );
+    std::cout << "  ( " << 512 << " ) -> " << a.id0 << " " << a.id1 << " " << a.id2 << std::endl;
+
+    std::cout << "Testando ID_t -> ID_b20" << std::endl;
+    ID_t b( 3, 2, 1 );
+    int c = Base::ID2int( &b );
+    std::cout << "  ( " << b.id0 << " " << b.id1 << " " << b.id2 << " ) -> " << c << std::endl;
+
     return 0;
 }
